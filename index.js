ea3e9d9aeaced85139f8504bf3f08e21f7a2be76
bill6653 <billcrs512@gmail.com>
2023-03-03 15:57:40 -0600

    corrected path in index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown');
+const generateMarkdown = require('./develop/utils/generateMarkdown');
 const licenses = ['None', 'MIT', 'BSD', 'GPL']
 // TODO: Create an array of questions for user input
 const questions = [
